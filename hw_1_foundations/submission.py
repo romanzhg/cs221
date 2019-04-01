0a096d836d4bf3f3713b97adad33d73bddccc6f4
Hong Zhang <hongzhang@HongdeiMac.local>
2019-04-02 01:08:05 +0800

    hw1

--- hw_1_foundations/submission.py
+++ hw_1_foundations/submission.py
@@ -12,7 +12,9 @@ def findAlphabeticallyLastWord(text):
     You might find max() and list comprehensions handy here.
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    wordList = text.split()
+    wordList.sort()
+    return max(wordList)
     # END_YOUR_CODE
 
 ############################################################
@@ -24,11 +26,61 @@ def euclideanDistance(loc1, loc2):
     are pairs of numbers (e.g., (3, 5)).
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return math.sqrt(math.pow(loc1[0] - loc2[0], 2) + math.pow(loc1[1] - loc2[1], 2))
     # END_YOUR_CODE
 
 ############################################################
 # Problem 3c
+
+def initListOfSet(size):
+    # Initialize a list of list. The outer list index starts from 0.
+    rtn = list()
+    for i in range(0,size):
+        rtn.append(set())
+    return rtn
+
+def maybeInsertWord(word, wordToId, wordCounter):
+    if word in wordToId:
+        return wordCounter
+    else:
+        wordToId[word] = wordCounter
+        return wordCounter + 1
+
+def buildGraph(sentence):
+    words = sentence.split()
+    sentenceLen = len(words)
+
+    wordToId = dict()
+    wordCount = 0
+
+    for word in words:
+        wordCount = maybeInsertWord(word, wordToId, wordCount)
+
+    idToWord = [None] * wordCount
+    for key, value in wordToId.iteritems():
+        idToWord[value] = key
+
+    graph = initListOfSet(wordCount)
+
+    for index in range(0, len(words) - 1):
+        wordFrom = words[index]
+        wordTo = words[index + 1]
+        graph[wordToId[wordFrom]].add(wordToId[wordTo])
+
+    return idToWord, graph, wordCount, sentenceLen
+
+def helper(rtnList, idToWord, graph, curVertexId, curSentence, remainingWords):
+    # TODO: if the curSentence is empty, should not return it.
+    if remainingWords == 1:
+        curSentence.append(idToWord[curVertexId])
+        rtnList.append(" ".join(curSentence))
+        curSentence.pop()
+        return
+    
+    curSentence.append(idToWord[curVertexId])
+    for adj in graph[curVertexId]:
+        helper(rtnList, idToWord, graph, adj, curSentence, remainingWords - 1)
+    curSentence.pop()
 
 def mutateSentences(sentence):
     """
@@ -50,7 +102,14 @@ def mutateSentences(sentence):
                 (reordered versions of this list are allowed)
     """
     # BEGIN_YOUR_CODE (our solution is 20 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    idToWord, graph, uniqueWords, sentenceLen = buildGraph(sentence)
+    rtnList = list()
+    curSentence = list()
+
+    for curId in range(uniqueWords):
+        helper(rtnList, idToWord, graph, curId, curSentence, sentenceLen)
+
+    return rtnList
     # END_YOUR_CODE
 
 ############################################################
@@ -64,7 +123,12 @@ def sparseVectorDotProduct(v1, v2):
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    rtn = float(0)
+    for key in v1:
+        if key in v2:
+            rtn += v1[key] * v2[key]
+
+    return rtn
     # END_YOUR_CODE
 
 ############################################################
@@ -76,7 +140,8 @@ def incrementSparseVector(v1, scale, v2):
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    for key in v2:
+        v1[key] += v2[key] * scale
     # END_YOUR_CODE
 
 ############################################################
@@ -89,11 +154,36 @@ def findSingletonWords(text):
     You might find it useful to use collections.defaultdict(int).
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    dd = collections.defaultdict(int)
+    for word in text.split():
+        dd[word] += 1
+    
+    rtn = set()
+    for key in dd:
+        if dd[key] == 1:
+            rtn.add(key)
+    return rtn
     # END_YOUR_CODE
 
 ############################################################
 # Problem 3g
+
+def getDp(text, dp, start, end):
+    if start == end:
+        return 1
+    if start > end:
+        return 0
+    if dp[start][end] != 0:
+        return dp[start][end]
+    if text[start] == text[end]:
+        rtn = 2 + getDp(text, dp, start + 1, end - 1)
+        dp[start][end] = rtn
+        return rtn
+    else:
+        rtn = max(getDp(text, dp, start + 1, end),
+                  getDp(text, dp, start, end - 1))
+        dp[start][end] = rtn
+        return rtn
 
 def computeLongestPalindromeLength(text):
     """
@@ -105,5 +195,7 @@ def computeLongestPalindromeLength(text):
     You should first define a recurrence before you start coding.
     """
     # BEGIN_YOUR_CODE (our solution is 19 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    textLen = len(text)
+    dp = [[0 for col in range(textLen)] for row in range(textLen)]
+    return getDp(text, dp, 0, textLen - 1)
     # END_YOUR_CODE
